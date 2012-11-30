7ac60fe3bc7271aea0d70dd4123ecf040c06a072
David Lu!! <davidvlu@gmail.com>
2012-11-30 01:31:45 +0000

    retooled teleop

--- precise_trajectory/pr2_kinect_teleop/src/teleop.py
+++ precise_trajectory/pr2_kinect_teleop/src/teleop.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('pr2_kinect_teleop')
 import rospy
-from pr2_trajectory import BOTH_ARMS, ARM_JOINTS
-from pr2_controllers_msgs.msg import *
-from pr2_gripper_sensor_msgs.msg import *
-from trajectory_msgs.msg import JointTrajectoryPoint
-from actionlib import SimpleActionClient, SimpleGoalState
+from pr2_precise_trajectory.arm_controller import ArmController, get_arm_joint_names
+from pr2_precise_trajectory.converter import simple_to_message
 from sensor_msgs.msg import JointState
-import trajectory_msgs.msg
+
+#For head motion
+from actionlib import SimpleActionClient, SimpleGoalState
+from trajectory_msgs.msg import JointTrajectoryPoint
+from pr2_controllers_msgs.msg import JointTrajectoryAction, JointTrajectoryGoal
 
 VELOCITY_WINDOW = 5
 
 class ArmJoint:
-    def __init__(self):
-        self.arms = {}
-        for arm in BOTH_ARMS:
-            self.arms[arm] = SimpleActionClient("%s_arm_controller/joint_trajectory_action"%arm, JointTrajectoryAction)
-            #wait for the action servers to come up 
-            self.arms[arm].wait_for_server()
+    def __init__(self, arms=['l', 'r']):
+        rospy.init_node('pr2_kinect_teleop')
+        self.arms = {}        
+        self.joint_histories = {}
+        for arm in arms:
+            self.arms[arm] = ArmController(arm) 
+            self.joint_histories[arm] = []
+
         head = SimpleActionClient("/head_traj_controller/joint_trajectory_action", JointTrajectoryAction)
         head.wait_for_server()
         goal = JointTrajectoryGoal()
@@ -28,16 +31,11 @@ class ArmJoint:
         head.send_goal(goal)
 
         self.joint_sub = rospy.Subscriber('/kinect_angles', JointState, self.joint_cb)
-        self.joint_histories = {'l': [], 'r': []}
-        self.names = {'l': ['l_%s'%a for a in ARM_JOINTS],
-                      'r': ['r_%s'%a for a in ARM_JOINTS]}
-
 
     def joint_cb(self, msg):
-        for arm in BOTH_ARMS:
+        for arm in self.arms:
             position = []
-            for joint in ARM_JOINTS:
-                name = "%s_%s"%(arm, joint)
+            for name in get_arm_joint_names(arm):
                 if name not in msg.name:
                     position.append(0.0)
                 else:
@@ -69,28 +67,15 @@ class ArmJoint:
         r = rospy.Rate(hz)
 
         while not rospy.is_shutdown():
-            for arm in ['l', 'r']:
-                # our goal variable
-                goal = JointTrajectoryGoal()
-                # Start the trajectory immediately
-                goal.trajectory.header.stamp = rospy.Time.now()
-
-                # First, the joint names, which apply to all waypoints
-                goal.trajectory.joint_names = self.names[arm]
-
-                # We will have N waypoints in this goal trajectory
-                goal.trajectory.points = [trajectory_msgs.msg.JointTrajectoryPoint()]
-
-                # First trajectory point
-                goal.trajectory.points[0].positions = self.get_positions(arm)
-                # goal.trajectory.points[0].velocities = self.get_velocities(arm)
-                # set time we want this trajectory to be reached at
-                goal.trajectory.points[0].time_from_start = rospy.Duration(1.0/hz)
-                self.arms[arm].send_goal(goal)
+            for arm in self.arms:
+                m = {}
+                m[arm] = self.get_positions(arm)
+                m['time'] = 1.0 / hz
+                trajectory = simple_to_message(m, arm)
+                self.arms[arm].start_trajectory(trajectory, wait=False)
             r.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('pr2_kinect_teleop')
     a = ArmJoint()
     a.spin()
 
