9ec45ccd2fa8f3c3e805f75ca3e90dc769be90f0
David V. Lu <davidvlu@gmail.com>
2015-03-12 13:38:15 -0700

    Fix race condition

--- pr2_score_creator/src/interactive_recorder.py
+++ pr2_score_creator/src/interactive_recorder.py
@@ -132,10 +132,10 @@ class InteractiveRecorder:
     def goto(self, delta):
         if self.busy:
             return
-        self.busy = True
         new_i = self.mi + delta
         if not self.score.is_valid_index(new_i):
             return
+        self.busy = True
         #m = self.score.get_state(new_i)
         m = self.score.get_keyframe(new_i, delta==-1)
 
